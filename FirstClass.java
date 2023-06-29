d6bfa211cece1e5ec99d94fa38ed1a02efae310d
samuelheel7 <138099191+samuelheel7@users.noreply.github.com>
2023-06-29 12:26:42 +0530

    Add files via upload
    it is 7th table up to 16th number.

---
+++ FirstClass.java
@@ -0,0 +1,22 @@
+
+public class FirstClass {
+
+	// 7th table
+
+	static void table() {
+
+		int n = 16;
+		for (int i = 1; i <= n; i++) {
+
+			System.out.println(i + "x" + "7" + "=" + i*7);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
+		FirstClass.table();
+		
+	}
+	
+}
